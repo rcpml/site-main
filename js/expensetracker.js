098659c5848d053083415877009b08d04c5e580b
Recep E <recepemul@gmail.com>
2020-03-22 21:31:33 +0300

    Expense Tracker up

--- js/expensetracker.js
+++ js/expensetracker.js
@@ -1,2 +1,40 @@
 "use strict";
 
+let totalExpense = 0;
+let totalIncome = 0;
+let totalAmount = 0;
+
+const userInput = document.getElementById('input-value-js');
+const expense = document.getElementById('expense-js');
+const income = document.getElementById('income-js');
+
+const expenseDisplay = document.getElementById('expense-display-js');
+const incomeDisplay = document.getElementById('income-display-js');
+const totalDisplay = document.getElementById('total-display-js');
+
+const submitButton = document.getElementById('submit-value-js');
+submitButton.addEventListener('click', submitValue);
+
+totalDisplay.textContent = `${totalAmount}$`;
+
+function updateAmount() {
+  if (expense.checked) {
+    totalExpense += parseInt(userInput.value);
+    console.log(`Expense updated: ${expense}`); 
+  } else {
+    totalIncome += parseInt(userInput.value);
+    console.log(`Income updated: ${income}`);
+  }
+}
+
+function updateDisplay() {
+  expenseDisplay.textContent = totalExpense;
+  incomeDisplay.textContent = totalIncome;
+  totalDisplay.textContent = (totalIncome - totalExpense);
+}
+
+function submitValue() {
+  updateAmount();
+  updateDisplay();
+  console.log("Submitted");
+}
